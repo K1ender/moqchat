4118085e8cd44c2ae72ed483301e92f537fe2cda
K1ender <146767945+K1ender@users.noreply.github.com>
2026-03-31 19:58:17 +0300

    feat(config): initialize configuration management

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"context"
+
+	"github.com/K1ender/moqchat/pkg/api"
+)
+
+func main() {
+	ctx := context.Background()
+
+	err := api.Run(ctx)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type Env string
+
+const (
+	Production  Env = "production"
+	Development Env = "development"
+)
+
+type Config struct {
+	Env Env
+	Tls Tls
+}
+
+type Tls struct {
+	CertPath string `env:"TLS_CERTIFICATE"`
+	KeyPath  string `env:"TLS_KEY"`
+}
+
+func MustInit() *Config {
+	var cfg Config
+
+	err := cleanenv.ReadEnv(&cfg)
+	if err == nil {
+		return &cfg
+	}
+
+	err = cleanenv.ReadConfig(".env", &cfg)
+	if err == nil {
+		return &cfg
+	}
+
+	panic(err)
+}
