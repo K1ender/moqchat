3f29de0465ad33951abc1c094e7e7b0729686169
K1ender <146767945+K1ender@users.noreply.github.com>
2026-04-01 12:17:56 +0300

    feat(dto): enforce password validation rules in CreateUserDTO.

--- internal/entity/dto/createUserDTO.go
+++ internal/entity/dto/createUserDTO.go
@@ -3,5 +3,5 @@ package dto
 type CreateUserDTO struct {
 	Username string `json:"username" validate:"required"`
 	Email    string `json:"email" validate:"required"`
-	Password string `json:"password" validate:"required"`
+	Password string `json:"password" validate:"required,min=6"`
 }
