9db4e4a5d7ebae6b5df38f6228e32b5692afe43e
K1ender <146767945+K1ender@users.noreply.github.com>
2026-04-17 21:57:27 +0300

    feat(session): implement session management functionality
    * Move session-related functions from auth to session service

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -2,9 +2,6 @@ package service
 
 import (
 	"context"
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
 	"time"
 
@@ -23,63 +20,17 @@ const (
 type Auth interface {
 	Login(ctx context.Context, email string, password string) (uuid.UUID, error)
 	Register(ctx context.Context, username string, email string, password string) (uuid.UUID, error)
-
-	CreateSession(ctx context.Context, userID uuid.UUID) (string, error)
-	GetUserIDFromToken(ctx context.Context, token string) (uuid.UUID, error)
-	GetSession(ctx context.Context, id uuid.UUID) (model.Session, error)
-	ExtendSession(ctx context.Context, id uuid.UUID) error
 }
 
 type AuthUsecase struct {
 	userRepo    repository.User
-	sessionRepo repository.Session
 }
 
-func NewAuthUsecase(userRepo repository.User, sessionRepo repository.Session) Auth {
-	return &AuthUsecase{userRepo: userRepo, sessionRepo: sessionRepo}
-}
-
-// CreateSession implements [Auth].
-func (a *AuthUsecase) CreateSession(ctx context.Context, userID uuid.UUID) (string, error) {
-	token, err := cryptoRandomString(RandomStringLength)
-	if err != nil {
-		return "", fmt.Errorf("failed to generate token: %w", err)
-	}
-
-	tokenHash := hashToken([]byte(token))
-
-	session := model.Session{UserID: userID, Token: tokenHash, ExpiresAt: time.Now().Add(SessionDuration)}
-	_, err = a.sessionRepo.CreateSession(ctx, session)
-	if err != nil {
-		return "", fmt.Errorf("failed to create session: %w", err)
-	}
-
-	return token, nil
-}
-
-// ExtendSession implements [Auth].
-func (a *AuthUsecase) ExtendSession(ctx context.Context, id uuid.UUID) error {
-	err := a.sessionRepo.UpdateExpiresAt(ctx, id, time.Now().Add(HalfSessionDuration))
-	if err != nil {
-		return fmt.Errorf("failed to extend session: %w", err)
-	}
-
-	return nil
-}
-
-// GetSession implements [Auth].
-func (a *AuthUsecase) GetSession(ctx context.Context, id uuid.UUID) (model.Session, error) {
-	session, err := a.sessionRepo.FindSessionByID(ctx, id)
-	if err != nil {
-		return model.Session{}, fmt.Errorf("failed to get session: %w", err)
-	}
-
-	return session, nil
+func NewAuthUsecase(userRepo repository.User) Auth {
+	return &AuthUsecase{userRepo: userRepo}
 }
 
 // Login implements [Auth].
-// TODO: decide whether Login should create a session
-// keeping it here simplifies API, but separating gives more control (e.g. stateless auth, different session strategies)
 func (a *AuthUsecase) Login(ctx context.Context, email string, password string) (uuid.UUID, error) {
 	user, err := a.userRepo.FindUserByEmail(ctx, email)
 	if err != nil {
@@ -94,7 +45,6 @@ func (a *AuthUsecase) Login(ctx context.Context, email string, password string) 
 }
 
 // Register implements [Auth].
-// TODO: decide whether Register should create a session
 func (a *AuthUsecase) Register(ctx context.Context, username string, email string, password string) (uuid.UUID, error) {
 	passwordHash, err := hashPassword([]byte(password))
 	if err != nil {
@@ -108,34 +58,6 @@ func (a *AuthUsecase) Register(ctx context.Context, username string, email strin
 	}
 
 	return id, nil
-}
-
-// GetUserIDFromToken implements [Auth].
-func (a *AuthUsecase) GetUserIDFromToken(ctx context.Context, token string) (uuid.UUID, error) {
-	tokenHash := hashToken([]byte(token))
-
-	session, err := a.sessionRepo.FindSessionByToken(ctx, tokenHash)
-	if err != nil {
-		return uuid.Nil, fmt.Errorf("failed to get session: %w", err)
-	}
-
-	return session.UserID, nil
-}
-
-func cryptoRandomString(length int) (string, error) {
-	bytes := make([]byte, length)
-	_, err := rand.Read(bytes)
-	if err != nil {
-		return "", fmt.Errorf("failed to generate random string: %w", err)
-	}
-
-	return hex.EncodeToString(bytes), nil
-}
-
-func hashToken(token []byte) string {
-	hashedToken := sha256.Sum256([]byte(token))
-	tokenHash := hex.EncodeToString(hashedToken[:])
-	return tokenHash
 }
 
 func hashPassword(password []byte) ([]byte, error) {
---
+++ internal/service/session.go
@@ -0,0 +1,109 @@
+package service
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/K1ender/moqchat/internal/entity/model"
+	"github.com/K1ender/moqchat/internal/repository"
+	"github.com/google/uuid"
+)
+
+type Session interface {
+	Create(ctx context.Context, userID uuid.UUID) (string, error)
+	GetUserIDFromToken(ctx context.Context, token string) (uuid.UUID, error)
+	Get(ctx context.Context, id uuid.UUID) (model.Session, error)
+	Extend(ctx context.Context, id uuid.UUID) error
+}
+
+type SessionUsecase struct {
+	sessionRepo repository.Session
+}
+
+func NewSessionUsecase(sessionRepo repository.Session) Session {
+	return &SessionUsecase{sessionRepo: sessionRepo}
+}
+
+var (
+	ErrSessionExpired = errors.New("session expired")
+)
+
+// CreateSession implements [Session].
+func (s *SessionUsecase) Create(ctx context.Context, userID uuid.UUID) (string, error) {
+	token, err := cryptoRandomString(RandomStringLength)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate token: %w", err)
+	}
+
+	tokenHash := hashToken([]byte(token))
+
+	session := model.Session{UserID: userID, Token: tokenHash, ExpiresAt: time.Now().Add(SessionDuration)}
+	_, err = s.sessionRepo.CreateSession(ctx, session)
+	if err != nil {
+		return "", fmt.Errorf("failed to create session: %w", err)
+	}
+
+	return token, nil
+}
+
+// Extend implements [Session].
+func (s *SessionUsecase) Extend(ctx context.Context, id uuid.UUID) error {
+	session, err := s.sessionRepo.FindSessionByID(ctx, id)
+	if err != nil {
+		return fmt.Errorf("failed to find session: %w", err)
+	}
+
+	err = s.sessionRepo.UpdateExpiresAt(ctx, id, session.ExpiresAt.Add(SessionDuration))
+	if err != nil {
+		return fmt.Errorf("failed to extend session: %w", err)
+	}
+
+	return nil
+}
+
+// Get implements [Session].
+func (s *SessionUsecase) Get(ctx context.Context, id uuid.UUID) (model.Session, error) {
+	session, err := s.sessionRepo.FindSessionByID(ctx, id)
+	if err != nil {
+		return model.Session{}, fmt.Errorf("failed to get session: %w", err)
+	}
+
+	return session, nil
+}
+
+// GetUserIDFromToken implements [Session].
+func (s *SessionUsecase) GetUserIDFromToken(ctx context.Context, token string) (uuid.UUID, error) {
+	tokenHash := hashToken([]byte(token))
+
+	session, err := s.sessionRepo.FindSessionByToken(ctx, tokenHash)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to get session: %w", err)
+	}
+
+	if session.ExpiresAt.Before(time.Now()) {
+		return uuid.Nil, ErrSessionExpired
+	}
+
+	return session.UserID, nil
+}
+
+func cryptoRandomString(length int) (string, error) {
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate random string: %w", err)
+	}
+
+	return hex.EncodeToString(bytes), nil
+}
+
+func hashToken(token []byte) string {
+	hashedToken := sha256.Sum256([]byte(token))
+	tokenHash := hex.EncodeToString(hashedToken[:])
+	return tokenHash
+}
